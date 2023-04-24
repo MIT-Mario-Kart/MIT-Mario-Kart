a48f43bfa62122750259a857beeef7c8311d24c4
Hugo Jeannin <hugo.jeannin@epfl.ch>
2023-04-24 19:51:54 +0200

    flow map pas fini

---
+++ FlowMaps/circuit20x20.py
@@ -0,0 +1,18 @@
+import math
+
+angles = [[0 for _ in range(20)] for _ in range(20)]
+
+a1 = 60
+a2 = 30
+a3 = 0
+a4 = 330
+a5 = 300
+a6 = 270
+a7 = 240
+a8 = 210
+a9 = 180
+a10 = 150
+a11 = 120
+a12 = 90
+
+
