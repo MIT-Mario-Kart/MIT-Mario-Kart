01fe257f807b6b951731889ad8e114fe61cd0d5a
therokdaba <therokdaba@gmail.com>
2023-05-22 16:11:44 +0200

    NEW: script to launch GUI

---
+++ Server/Miscellanous/launch_gui.py
@@ -0,0 +1,8 @@
+import socket
+
+serverAddrPort = ("172.20.10.6", 8899                   )
+bufferSize = 1024
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect(serverAddrPort)
+    s.sendall(b"GUI")
