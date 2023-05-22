6d51bf6a265827ee06527740d179cf0bbbde1672
Hugo Jeannin <hugo.jeannin@epfl.ch>
2023-05-22 15:08:19 +0200

    grid

--- Server/Algo/Grid.py
+++ Server/Algo/Grid.py
@@ -12,8 +12,8 @@ class Grid:
         self.height = 200
 
     def getCircuitCoords(self, x, y):
-        new_X = (self.bot_right[0] - x) / abs(self.bot_right[0] - self.bot_left[0]) * self.width
-        new_Y = ((y - self.bot_right[1]) / abs(self.bot_right[1] - self.top_right[1]) * self.height) + 16
+        new_X = ((self.bot_right[0] - x) / abs(self.bot_right[0] - self.bot_left[0]) * self.width) - 5
+        new_Y = ((y - self.bot_right[1]) / abs(self.bot_right[1] - self.top_right[1]) * self.height) + 10
         print(self.bot_right[1], y, self.top_right[1])
         return new_X, new_Y
     
