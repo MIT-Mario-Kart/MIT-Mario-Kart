80ef18bffb8d664573c4f12e52827c8028e1fefa
Thomas Kemper <thomas.kemper@epfl.ch>
2023-06-08 10:34:24 +0200

    change the position of the cars

--- Server/main.py
+++ Server/main.py
@@ -13,8 +13,8 @@ def launch_server(control):
 # create all car objects corresponding to the real life cars
 car1 = Car("CAR1", 1, "CAR_ID_1", GREEN_C, color="green", x=160, y=20, orientation=180, ai=True)
 car2 = Car("CAR2", 2, "CAR_ID_2", RED_C, color="red", x=140, y=20, orientation=180, ai=False)
-car3 = Car("CAR3", 2, "CAR_ID_3", ROSE_C, color="pink", x=140, y=20, orientation=180, ai=True)
-car4 = Car("CAR4", 2, "CAR_ID_4", BLUE_C, color="blue", x=140, y=20, orientation=180, ai=False)
+car3 = Car("CAR3", 2, "CAR_ID_3", ROSE_C, color="pink", x=100, y=20, orientation=180, ai=True)
+car4 = Car("CAR4", 2, "CAR_ID_4", BLUE_C, color="blue", x=120, y=20, orientation=180, ai=False)
 
 
 car_list = [car1, car2, car3, car4]
